0d80d11155deb2ea78600f87204426d33525eb17
JES20 <dore.joel.dore@gmail.com>
2020-10-16 07:46:41 -0400

    Update getCorrectAnswer function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,6 @@
 const timerEl = document.getElementById('timer');
 let currentScore = 0;
+let currentQ = 0;
 
 // Move to next div # from current div #
 function changeDiv(curr, next) {
@@ -21,25 +22,58 @@ function startTimer() {
         () => {
             secondsLeft--;
             timerEl.textContent = secondsLeft;
-            if (secondsLeft === 0) {
+            if (secondsLeft <= 0) {
                 clearInterval(timerInterval);
                 endGame();
             }
         }, 1000);
 };
 
-function handleAnswerClick() {
-    // button onclick --> this function
-    // let correctAnswer = getCorrectAnswer();
+function nextQuestion() {
+    // Populate question-container w/ next questionBank[currentQ]
+}
 
+function handleAnswerClick(event) {
+    // let correctAnswer = getCorrectAnswer(currentQ);
+    // if event.target.textContent===correctAnswer,
+    // // currentScore += 10;
+    // // event.target.classList.add(.correct)
+    // else 
+    // // secondsLeft -=10;
+    // // event.target.classList.add(.wrong)
+    // setTimeout(-move to next question-, 500)
 };
 
-function getCorrectAnswer(currQ) {
+function getCorrectAnswer(currentQ) {
+    let arr = questionBank[currentQ].answersArray;
     // loop through answersArray, identify correct answer
-    // retrieve correct answer from questionBank.
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j].correct) {
+            // return correct answer.
+            return arr[j].answer
+        }
+    }
 };
 
 function endGame() {
-    // Log currentScore in #final-score
-    changeDiv('question-container', 'results-page')
+    // Log currentScore in #final-score text
+    // set currentScore = 0;
+    // changeDiv('question-container', 'results-page');
 }
+
+
+
+/*--------------------------------*/
+
+
+// const arr = [{age: 3, name: 'Jim'}, {age: 4, name: 'Joe'}]
+
+// arr.sort(function(current, next) {
+//     if (current.age > next.age) {
+//         return 1
+//     } else if (current.age < next.age) {
+//         return -1
+//     } else {
+//         return 0
+//     }
+// })
