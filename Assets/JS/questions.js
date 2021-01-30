6b3f33f7f726152da848fb35e398c71226329fed
JES20 <dore.joel.dore@gmail.com>
2021-01-30 14:55:26 -0500

    Update questions

--- Assets/JS/questions.js
+++ Assets/JS/questions.js
@@ -3,45 +3,46 @@ const questionBank = [
     {
         question: 'Which is NOT a primitive data type?',
         answersArray: [
+            { answer: 'array', correct: true },  // Correct
             { answer: 'string', correct: false },
-            { answer: 'array', correct: true },
             { answer: 'boolean', correct: false },
             { answer: 'undefined', correct: false }]
     },
 
-    {
-        question: 'Which of these statements is NOT equivalent to the others?',
+    {// Updated 1/30/21
+        question: 'Which of these array methods returns an array?',
         answersArray: [
-            { answer: 'i++', correct: false },
-            { answer: 'i =+ 1', correct: true },
-            { answer: 'i += 1', correct: false },
-            { answer: 'i = i + 1', correct: false }]
+            { answer: 'Array.splice()', correct: true }, // Correct
+            { answer: 'Array.forEach()', correct: false },
+            { answer: 'Array.find()', correct: false },
+            { answer: 'Array.reduce()', correct: false }]
     },
 
-    {
-        question: 'What is the function of `array.pop()`?',
+    { // Updated 1/30/21
+        question: 'What is the function of `Array.unshift()`?',
         answersArray: [
-            { answer: 'Removes first element of array', correct: false },
-            { answer: 'Removes last element of array', correct: true },
+            { answer: 'Removes first element of array', correct: true },  // Correct
+            { answer: 'Removes last element of array', correct: false },
             { answer: 'Adds element to end of array', correct: false },
             { answer: 'Adds element to start of array', correct: false }]
     },
 
     {
-        question: 'Which statement returns the number 2?',
+        question: 'Which of these statements returns the number 2?',
         answersArray: [
+            { answer: 'parseInt("2.9")', correct: true },  // Correct
             { answer: 'Math.floor(1.9)', correct: false },
             { answer: 'Math.round(2.9)', correct: false },
-            { answer: 'parseInt("2.9")', correct: true },
             { answer: 'parseFloat("2.9")', correct: false }]
     },
 
     {
-        question: 'Which is a JavaScript comparison operator?',
-        answersArray: [{ answer: '=', correct: false },
-        { answer: '!==', correct: true },
-        { answer: '+=', correct: false },
-        { answer: '--', correct: false }]
+        question: 'Which of the following is a comparison operator?',
+        answersArray: [
+            { answer: '!==', correct: true },  // Correct
+            { answer: '=', correct: false },
+            { answer: '+=', correct: false },
+            { answer: '--', correct: false }]
     }
 
 ];
